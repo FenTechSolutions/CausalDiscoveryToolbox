f808aebea6315075cbd3e9bf056357ce2886fae6
Diviyan Kalainathan <dkalainathan@gmail.com>
2017-07-03 15:23:11 +0200

    Added FSGNN

---
+++ cdt/deconvolution/model.py
@@ -0,0 +1,23 @@
+"""
+Pairwise causal models base class
+Author: Olivier Goudet
+Date : 7/06/2017
+"""
+
+
+class DeconvolutionModel(object):
+    """ Base class for all deconvolution models"""
+
+    def __init__(self):
+        """ Init. """
+        super(DeconvolutionModel, self).__init__()
+
+    def predict(self, df_data):
+        """ get the skeleton of the graph from raw data
+        :param df_data: data to construct a graph from
+        """
+        return self.create_skeleton_from_data(df_data)
+
+    def create_skeleton_from_data(self, data):
+
+        raise NotImplementedError
