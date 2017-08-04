c703704e698174a03c7b0edd4430ab6ffc23011a
Diviyan Kalainathan <dkalainathan@gmail.com>
2017-08-04 23:02:03 +0200

    Added Skeleton for pairwise functions

--- cdt/causality/pairwise_models/GPI.py
+++ cdt/causality/pairwise_models/GPI.py
@@ -1,1 +1,10 @@
 # ToDo : Implement
+from .model import Pairwise_Model
+
+
+class GPI(Pairwise_Model):
+    def __init__(self):
+        super(GPI, self).__init__()
+
+    def predict_proba(self, a, b):
+        return 0
--- cdt/causality/pairwise_models/IGCI.py
+++ cdt/causality/pairwise_models/IGCI.py
@@ -1,1 +1,10 @@
 # ToDo : Implement
+from .model import Pairwise_Model
+
+
+class IGCI(Pairwise_Model):
+    def __init__(self):
+        super(IGCI, self).__init__()
+
+    def predict_proba(self, a, b):
+        return 0
