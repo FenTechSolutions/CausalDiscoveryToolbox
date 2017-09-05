6f316a2c6f61f108b36fce8692455edade92e993
Diviyan Kalainathan <dkalainathan@gmail.com>
2017-09-05 15:58:26 +0200

    Adding PC + R packages

--- cdt/utils/R.py
+++ cdt/utils/R.py
@@ -4,11 +4,15 @@ from .Settings import SETTINGS
 
 class DefaultRPackages(object):
     __slots__ = ("pcalg",
-                 "minet")
+                 "kpcalg",
+                 "minet",
+                 "bnlearn")
 
     def __init__(self):  # Define here the default values of the parameters
         self.pcalg = None
         self.minet = None
+        self.kpcalg = None
+        self.bnlearn = None
 
 
 def load_r_wrapper():
@@ -22,6 +26,8 @@ def load_r_wrapper():
             rpy2.robjects.numpy2ri.activate()
             RPackages.minet = importr('minet')
             RPackages.pcalg = importr("pcalg")
+            RPackages.kpcalg = importr("kpcalg")
+            RPackages.bnlearn = importr("bnlearn")
             SETTINGS.r_is_available = True
         except rpy2.rinterface.RRuntimeError as e:
             SETTINGS.r_is_available = False
@@ -29,6 +35,10 @@ def load_r_wrapper():
     except ImportError as e:
         SETTINGS.r_is_available = False
         warnings.warn("R wrapper is not available : {}".format(e))
+
+
+def default_translation(rname):
+    return rname.replace('.', '_')
 
 RPackages = DefaultRPackages()
 load_r_wrapper()
