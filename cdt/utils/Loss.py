ffe71f857d108b16b4b087299223a489e02e7b27
Diviyan Kalainathan <dkalainathan@gmail.com>
2017-09-16 20:01:29 +0200

    Tentative fix Fourier

--- cdt/utils/Loss.py
+++ cdt/utils/Loss.py
@@ -17,28 +17,27 @@ def median_heursitic(m):
         for j in range(m.shape[0]):
             dist = 0
             for d in range(m.shape[1]):
-                dist += (m[i,d]-m[j,d])**2
+                dist += (m[i, d] - m[j, d]) ** 2
             list_dist.append(dist)
 
-    return 1/(2*np.median(list_dist))
+    return 1 / (2 * np.median(list_dist))
 
 
 def MMD_loss_tf(xy_true, xy_pred, gamma, low_memory_version=False):
-
     N, _ = xy_pred.get_shape().as_list()
 
     X = tf.concat([xy_pred, xy_true], 0)
     XX = tf.matmul(X, tf.transpose(X))
     X2 = tf.reduce_sum(X * X, 1, keep_dims=True)
-    exponent = -2*XX + X2  + tf.transpose(X2)
+    exponent = -2 * XX + X2 + tf.transpose(X2)
 
     s1 = tf.constant(1.0 / N, shape=[N, 1])
     s2 = -tf.constant(1.0 / N, shape=[N, 1])
     s = tf.concat([s1, s2], 0)
     S = tf.matmul(s, tf.transpose(s))
-    
+
     loss = 0
-    
+
     for i in gamma:
         kernel_val = tf.exp(-i * exponent)
         loss += tf.reduce_sum(S * kernel_val)
@@ -47,7 +46,6 @@ def MMD_loss_tf(xy_true, xy_pred, gamma, low_memory_version=False):
 
 
 class MMD_loss_th(th.nn.Module):
-
     def __init__(self, input_size, cuda=False):
         super(MMD_loss_th, self).__init__()
         self.bandwiths = [0.01, 0.1, 1, 5, 20, 50, 100]
@@ -56,11 +54,11 @@ class MMD_loss_th(th.nn.Module):
             s1 = th.cuda.FloatTensor(input_size, 1).fill_(1)
             s2 = s1.clone()
             s = th.cat([s1.div(input_size),
-                           s2.div(-input_size)], 0)
+                        s2.div(-input_size)], 0)
 
         else:
             s = th.cat([(th.ones([input_size, 1])).div(input_size),
-            (th.ones([input_size, 1])).div(-input_size)], 0)
+                        (th.ones([input_size, 1])).div(-input_size)], 0)
 
         self.S = s.mm(s.t())
         self.S = Variable(self.S)
@@ -72,7 +70,7 @@ class MMD_loss_th(th.nn.Module):
             X = th.cat([var_input, var_pred], 0)
         else:
             X = th.cat([th.cat([var_input, var_pred], 1),
-                th.cat([var_input, var_true], 1)], 0)
+                        th.cat([var_input, var_true], 1)], 0)
         # dot product between all combinations of rows in 'X'
         XX = X.mm(X.t())
 
@@ -96,39 +94,40 @@ class MMD_loss_th(th.nn.Module):
         return lossMMD.sqrt()
 
 
-def rp(k,s,d):
-
-  return tf.transpose(tf.concat([tf.concat([2*s*tf.random_normal([k,d], mean=0, stddev=1)], axis = 0), tf.random_uniform([k,1], minval=0, maxval=2*np.pi)], axis = 1))
-
-def f1(x,wz,N):
-
-  ones = tf.ones((N, 1))
-  x_ones = tf.concat([x, ones], axis = 1)
-  mult = tf.matmul(x_ones,wz)
-
-  return tf.cos(mult)
-
-def Fourier_MMD_Loss_tf(xy_true, xy_pred,nb_vectors_approx_MMD, gamma):
-
-  N, nDim = xy_pred.get_shape().as_list()
-
-  wz = rp(nb_vectors_approx_MMD, gamma, nDim)
-
-  e1 = tf.reduce_mean(f1(xy_true,wz,N), axis =0)
-  e2 = tf.reduce_mean(f1(xy_pred,wz,N), axis =0)
-
-  return tf.reduce_mean((e1 - e2)**2)
+def rp(k, s, d):
+    return tf.transpose(tf.concat([tf.concat([2 * si * tf.random_normal([k, d], mean=0, stddev=1) for si in s], axis=0),
+                                   tf.random_uniform([k * len(s), 1], minval=0, maxval=2 * np.pi)], axis=1))
 
 
-def MomentMatchingLoss_tf(xy_true, xy_pred, nb_moment = 1):
+def f1(x, wz, N):
+    ones = tf.ones((N, 1))
+    x_ones = tf.concat([x, ones], axis=1)
+    mult = tf.matmul(x_ones, wz)
+
+    return tf.cos(mult)
+
+
+def Fourier_MMD_Loss_tf(xy_true, xy_pred, nb_vectors_approx_MMD):
+    bandwiths_gamma = [0.005, 0.05, 0.25, 0.5, 1, 5, 50]
+    N, nDim = xy_pred.get_shape().as_list()
+
+    wz = rp(nb_vectors_approx_MMD, bandwiths_gamma, nDim)
+
+    e1 = tf.reduce_mean(f1(xy_true, wz, N), axis=0)
+    e2 = tf.reduce_mean(f1(xy_pred, wz, N), axis=0)
+
+    return tf.reduce_mean((e1 - e2) ** 2)
+
+
+def MomentMatchingLoss_tf(xy_true, xy_pred, nb_moment=1):
     """ k-moments loss, k being a parameter. These moments are raw moments and not normalized
 
     """
     loss = 0
     for i in range(1, nb_moment):
-        mean_pred = tf.reduce_mean(xy_pred**i, 0)
-        mean_true = tf.reduce_mean(xy_true**i, 0)
-        loss += tf.sqrt(tf.reduce_sum((mean_true - mean_pred)**2))  # L2
+        mean_pred = tf.reduce_mean(xy_pred ** i, 0)
+        mean_true = tf.reduce_mean(xy_true ** i, 0)
+        loss += tf.sqrt(tf.reduce_sum((mean_true - mean_pred) ** 2))  # L2
 
     return loss
 
@@ -137,6 +136,7 @@ class MomentMatchingLoss_th(th.nn.Module):
     """ k-moments loss, k being a parameter. These moments are raw moments and not normalized
 
     """
+
     def __init__(self, n_moments=1):
         """ Initialize the loss model
 
@@ -158,7 +158,7 @@ class MomentMatchingLoss_th(th.nn.Module):
             mk_pred = th.mean(th.pow(pred, i), 0)
             mk_tar = th.mean(th.pow(target, i), 0)
 
-            loss.add_(th.mean((mk_pred - mk_tar)**2))  # L2
+            loss.add_(th.mean((mk_pred - mk_tar) ** 2))  # L2
 
         return loss
 
