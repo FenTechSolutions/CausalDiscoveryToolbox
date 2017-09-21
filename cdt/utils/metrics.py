Fixed and rewritten Precision recall score

cdt/utils/metrics.py
@@ -5,7 +5,9 @@ Date : 20/09
 """
 
 import numpy as np
-from sklearn.metrics import auc
+import pandas as pd
+from sklearn.metrics import auc, precision_recall_curve
+
 
 def count_true(tp, fp):
     try:
@@ -41,57 +43,28 @@ def precision_recall(predictions, result):
     if type(predictions) != list:
         predictions = [predictions]
 
+    true_labels, true_nodes = result.adjacency_matrix()
+
     for pred in predictions:
         edges = pred.list_edges(descending=True, return_weights=True)
-        true_edges = result.list_edges()
-        p = len(true_edges)  # number of positives
+        m, nodes = pred.adjacency_matrix()
+        predictions = pd.DataFrame(m, columns=nodes)
+        if not set(true_nodes) == set(nodes):
+            for i in (set(true_nodes) - set(nodes)):
+                predictions[i] = 0
+                predictions.loc[len(predictions)] = 0
 
-        noedges = []
-        # Detect non-oriented edges
+        # Detect non-oriented edges and set a low value
+        set_value_no = np.min(predictions.values[np.nonzero(predictions.values)])/2
+
         for e in edges:
             if [e[1], e[0], e[2]] in edges:
-                e.append(noedges)
-                edges.remove(e)
-                edges.remove([e[1], e[0], e[2]])
+                predictions.loc[e[1], e[0]] = set_value_no
+                predictions.loc[e[1], e[0]] = set_value_no
 
-        tp = []
-        fp = []
-        previous_score = -1  # To detect if multiple edges have the same score
-        count_multiple = 1  # No of edges that have the same score
-
-        for e in edges:
-            if previous_score == e[3]:
-                count_multiple += 1
-            else:
-                count_multiple = 1
-
-            if [e[0], e[1]] in true_edges:
-                tp, fp = count_true(tp, fp)
-
-            else:
-                tp, fp = count_false(tp, fp)
-
-            previous_score = e[3]
-            if count_multiple > 1:  # suboptimal here
-                tp[-count_multiple:] = [tp[-1] for i in range(count_multiple)]
-                fp[-count_multiple:] = [fp[-1] for i in range(count_multiple)]
-
-        tpnoe, fpnoe = tp, fp
-        for noe in noedges:
-            if [noe[0], noe[1]] in true_edges or [noe[0], noe[1]] in true_edges:
-                tpnoe, fpnoe = count_true(tpnoe, fpnoe)
-                tpnoe, fpnoe = count_false(tpnoe, fpnoe)
-
-            else:
-                tpnoe, fpnoe = count_false(tpnoe, fpnoe)
-                tpnoe, fpnoe = count_false(tpnoe, fpnoe)
-
-        tp.extend([tpnoe[-1] for i in range(len(noedges)*2)])
-        fp.extend([fpnoe[-1] for i in range(len(noedges)*2)])
-
-        precision = [i/p for i in tp]
-        recall = [i/(i+j) for i, j in zip(tp, fp)]
-        aupr = auc(recall, precision)
+        predictions = predictions[true_nodes].as_matrix()
+        precision, recall, _ = precision_recall_curve(true_labels.reshape(-1), predictions.reshape(-1))
+        aupr = auc(recall, precision, reorder=True)
         out.append([aupr, precision, recall])
 
     return out