74e6c9e763807b9e0763837980ba7ada9ce8bc0a
Diviyan Kalainathan <dkalainathan@gmail.com>
2017-11-02 10:37:45 +0100

    Fix torch function import

--- cdt/generators/generators.py
+++ cdt/generators/generators.py
@@ -4,7 +4,7 @@ Date : 30/06/17
 """
 import numpy as np
 import tensorflow as tf
-from ..utils.Loss import MomentMatchingLoss_th as MomentMatchingLoss, MMD_loss_tf as MMD
+from ..utils.Loss import MMD_loss_tf as MMD
 from ..utils.Settings import SETTINGS, CGNN_SETTINGS
 from sklearn.linear_model import LassoLars
 from sklearn.svm import SVR
@@ -13,6 +13,7 @@ th = SETTINGS.torch
 
 if th is not None:
     from torch.autograd import Variable
+    from ..utils.Loss import MomentMatchingLoss_th as MomentMatchingLoss
 
     class SEMModel(th.nn.Module):
         """ Model for SEM regression/generation - Generation one-by-one
