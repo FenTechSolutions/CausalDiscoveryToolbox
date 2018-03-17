0f5414ff9c57ce130c8a1b1512c43353e21287b7
Diviyan Kalainathan <dkalainathan@gmail.com>
2018-03-17 16:46:11 +0100

    Renamed formats to read_file

--- cdt/__init__.py
+++ cdt/__init__.py
@@ -1,21 +1,15 @@
-"""
-The Causal Discovery Toolbox contains various methods for graph structure recovery and causal inference.
+"""The Causal Discovery Toolbox contains various methods for graph structure recovery and causal inference.
+
 Additionally, it offers many utilities for graphs types.
 It is CUDA-compatible for the most computationally expensive algorithms.
 """
 
-import tensorflow as tf  # Required import or TensorFlow will crash with PyTorch
-import os
-import cdt.causality
-import cdt.independence
+# import cdt.causality
+# import cdt.independence
 import cdt.generators
-from cdt.utils.Graph import DirectedGraph, UndirectedGraph
-from cdt.utils import Loss
-from cdt.utils.Settings import SETTINGS, CGNN_SETTINGS
+# from cdt.utils import Loss
+from cdt.utils.Settings import SETTINGS
 import cdt.utils.R
 from cdt.utils.R import RPackages
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # avoid the thousands of stderr output lines
-
-__all__ = ['DirectedGraph', 'UndirectedGraph']
-
-del cdt.cdt
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # avoid the thousands of stderr output lines
+# Dropping support of tensorflow
