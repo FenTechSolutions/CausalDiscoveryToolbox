250f0d90070d91d4e7dc2b81b35c2dc1c387c4de
Diviyan Kalainathan <dkalainathan@gmail.com>
2018-03-24 16:35:02 +0100

    Removed old generator class + Removed unecessary settings to fit torch

--- cdt/utils/Settings.py
+++ cdt/utils/Settings.py
@@ -8,6 +8,7 @@ import ast
 import os
 import warnings
 import multiprocessing
+import torch as th
 
 
 def message_warning(msg, *a):
@@ -33,71 +34,13 @@ class ConfigSettings(object):
         super(ConfigSettings, self).__init__()
         self.NB_JOBS = 8
         self.GPU = True
-        self.NB_GPU = 4
-        self.GPU_OFFSET = 0
-        self.GPU_LIST = [i for i in range(
-            self.GPU_OFFSET, self.GPU_OFFSET + self.NB_GPU)]
+        self.GPU_LIST = []
         self.autoset_config = True
         self.verbose = True
         self.r_is_available = False
 
         if self.autoset_config:
             self = autoset_settings(self)
-
-# DEPRECATED :  The CGNN SETTINGS will be integrated in the CGNN file.
-
-
-# class CGNNSettings(object):
-#     __slots__ = ("h_layer_dim",
-#                  "train_epochs",
-#                  "test_epochs",
-#                  "NB_RUNS",
-#                  "NB_MAX_RUNS",
-#                  "learning_rate",
-#                  "init_weights",
-#                  "ttest_threshold",
-#                  "nb_max_loop",
-#                  "kernel",
-#                  "nb_run_feature_selection",
-#                  "regul_param",
-#                  "nb_epoch_train_feature_selection",
-#                  "nb_epoch_eval_weights",
-#                  "use_Fast_MMD",
-#                  "nb_vectors_approx_MMD",
-#                  "complexity_graph_param",
-#                  "max_nb_points",
-#                  "max_parents_block",
-#                  "asymmetry_param",
-#                  "threshold_UMG")
-#
-#     def __init__(self):
-#         super(CGNNSettings, self).__init__()
-#         self.NB_RUNS = 8
-#         self.NB_MAX_RUNS = 32
-#         self.learning_rate = 0.01
-#         self.init_weights = 0.05
-#         self.max_nb_points = 1500
-#         self.h_layer_dim = 20
-#         self.use_Fast_MMD = False
-#         self.nb_vectors_approx_MMD = 100
-#         self.threshold_UMG = 0.15
-#         self.train_epochs = 1000
-#         self.test_epochs = 500
-#         self.complexity_graph_param = 0.00005
-#         self.ttest_threshold = 0.01
-#         self.nb_max_loop = 3
-#
-#         self.kernel = "RBF"
-#
-#         # specific for FSGNN
-#         self.nb_run_feature_selection = 1
-#         self.nb_epoch_train_feature_selection = 2000
-#         self.nb_epoch_eval_weights = 500
-#         self.regul_param = 0.004
-#
-#         # specific for blockwise CGNN
-#         self.max_parents_block = 5
-#         self.asymmetry_param = 0.0001
 
 
 def autoset_settings(set_var):
@@ -107,19 +50,24 @@ def autoset_settings(set_var):
     :param set_var: Variable to set. Must be of type ConfigSettings
     """
     try:
+        if th.cuda.is_available():
+            set_var.GPU = True
         devices = ast.literal_eval(os.environ["CUDA_VISIBLE_DEVICES"])
         if type(devices) != list and type(devices) != tuple:
             devices = [devices]
         if len(devices) != 0:
-            set_var.GPU_LIST = devices
-            set_var.GPU = True
+            set_var.GPU_LIST = list(range(len(devices)))
             set_var.NB_JOBS = len(devices)
-            print("Detecting CUDA devices : {}".format(devices))
+
+        elif set_var.GPU:
+            set_var.GPU_LIST = [0]
         else:
             raise KeyError
+        print("Detecting CUDA devices : {}".format(devices))
+
     except KeyError:
         warnings.warn(
-            "No GPU automatically detected. Setting SETTINGS.GPU to false," +
+            "No GPU automatically detected." +
             "SETTINGS.GPU_LIST to [], and SETTINGS.NB_JOBS to cpu_count.")
         set_var.GPU = False
         set_var.GPU_LIST = []
--- cdt/utils/__init__.py
+++ cdt/utils/__init__.py
@@ -1,1 +1,3 @@
-from .read_file import read_causal_pairs, read_adjacency_matrix, read_list_edges
+from .read_file import (read_causal_pairs,
+                        read_adjacency_matrix,
+                        read_list_edges)
