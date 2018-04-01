6207520272a3a60b40f8accdd91ce7f7457503a1
Diviyan Kalainathan <dkalainathan@gmail.com>
2018-04-01 13:50:22 +0200

    Added GES algorithm.

---
+++ cdt/causality/graph/R_templates/ges.R
@@ -0,0 +1,18 @@
+library(methods)
+library(pcalg)
+
+dataset <- read.csv(file='{FILE}', sep=",");
+
+if({SKELETON}){
+  fixedGaps <- read.csv(file='{GAPS}', sep=",", header=FALSE) # NULL
+  fixedGaps = (data.matrix(fixedGaps))
+  rownames(fixedGaps) <- colnames(fixedGaps)
+}else{
+  fixedGaps = NULL
+}
+score <- new("{SCORE}", data = dataset)
+result <- pcalg::ges(score, fixedGaps=fixedGaps)
+gesmat <- as(result$essgraph, "matrix")
+gesmat[gesmat] <- 1
+  #gesmat[!gesmat] <- 0
+write.csv(gesmat, row.names=FALSE, file = '{OUTPUT}');
