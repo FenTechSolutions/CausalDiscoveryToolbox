e9ed1473a8b7906f7296f31a96c865bca4aedaad
Diviyan Kalainathan <dkalainathan@gmail.com>
2018-04-03 00:17:39 +0200

    Started pytest tests.

---
+++ tests/scripts/test_independence_stats.py
@@ -0,0 +1,22 @@
+"""Test stat methods."""
+
+
+import os
+import pandas as pd
+import networkx as nx
+from cdt.independence.stats import (AdjMI, NormMI, PearsonCorrelation,
+                                    SpearmanCorrelation, KendallTau,
+                                    NormalizedHSIC, MIRegression)
+
+
+def init():
+    return pd.read_csv('{}/../datasets/Example_graph_numdata.csv'.format(os.path.dirname(os.path.realpath(__file__))))
+
+
+def test_statistical_methods():
+    data = init()
+    for method in [AdjMI, NormMI, PearsonCorrelation, SpearmanCorrelation,
+                   KendallTau, NormalizedHSIC, MIRegression]:
+        model = method()
+        assert type(model.predict_undirected_graph(data)) == nx.Graph
+        assert model.predict(data.iloc[:, 0], data.iloc[:, 1]) != model.predict(data.iloc[:, 0], data.iloc[:, 0])
