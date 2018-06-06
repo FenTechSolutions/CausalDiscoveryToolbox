[FIX] Loss for more numerical stability + CGNN Graph Management

cdt/causality/graph/CGNN.py
@@ -51,21 +51,20 @@ class CGNN_block(th.nn.Module):
 class CGNN_model(th.nn.Module):
     """Class for one CGNN instance."""
 
-    def __init__(self, graph, batch_size, nh=20, gpu=None,
+    def __init__(self, adj_matrix, batch_size, nh=20, gpu=None,
                  gpu_id=0, confounding=False, initial_graph=None, **kwargs):
         """Init the model by creating the blocks and extracting the topological order."""
         super(CGNN_model, self).__init__()
         gpu = SETTINGS.get_default(gpu=gpu)
         device = 'cuda:{}'.format(gpu_id) if gpu else 'cpu'
-        nodes = list(graph.nodes())
-        self.topological_order = [nodes.index(i) for i in nx.topological_sort(graph)]
-        self.adjacency_matrix = nx.adj_matrix(graph, weight=None).todense()
+        self.topological_order = [i for i in nx.topological_sort(nx.DiGraph(adj_matrix))]
+        self.adjacency_matrix = adj_matrix
         self.confounding = confounding
         if initial_graph is None:
-            initial_graph = graph
+
             self.i_adj_matrix = self.adjacency_matrix
         else:
-            self.i_adj_matrix = nx.adj_matrix(initial_graph, weight=None).todense()
+            self.i_adj_matrix = initial_graph
         self.blocks = th.nn.ModuleList()
         self.generated = [None for i in range(self.adjacency_matrix.shape[0])]
         self.noise = th.zeros(batch_size, self.adjacency_matrix.shape[0]).to(device)
@@ -103,12 +102,14 @@ class CGNN_model(th.nn.Module):
         verbose = SETTINGS.get_default(verbose=verbose)
         optim = th.optim.Adam(self.parameters(), lr=lr)
         self.score.zero_()
-
+    
         for epoch in range(train_epochs + test_epochs):
+            optim.zero_grad()
             generated_data = self.forward()
             mmd = self.criterion(generated_data, data)
             if verbose and not epoch % 200:
-                print("IDX: {}, MMD Score: {}".format(idx, mmd.item()))
+                
+                print("IDX: {}, Epoch: {}, MMD Score: {}".format(idx, epoch, mmd.item()))
             mmd.backward()
             optim.step()
             if epoch >= test_epochs:
@@ -117,24 +118,24 @@ class CGNN_model(th.nn.Module):
         return self.score.cpu().numpy() / test_epochs
 
 
-def graph_evaluation(data, graph, gpu=None, gpu_id=0, **kwargs):
+def graph_evaluation(data, adj_matrix, gpu=None, gpu_id=0, **kwargs):
     """Evaluate a graph taking account of the hardware."""
     gpu = SETTINGS.get_default(gpu=gpu)
     device = 'cuda:{}'.format(gpu_id) if gpu else 'cpu'
     obs = Variable(th.FloatTensor(data)).to(device)
-    cgnn = CGNN_model(graph, data.shape[0], gpu_id=gpu_id, **kwargs).to(device)
+    cgnn = CGNN_model(adj_matrix, data.shape[0], gpu_id=gpu_id, **kwargs).to(device)
 
     return cgnn.run(obs, **kwargs)
 
 
-def parallel_graph_evaluation(data, graph, nb_runs=16,
+def parallel_graph_evaluation(data, adj_matrix, nb_runs=16,
                               nb_jobs=None, **kwargs):
     """Parallelize the various runs of CGNN to evaluate a graph."""
     nb_jobs = SETTINGS.get_default(nb_jobs=nb_jobs)
     if nb_runs == 1:
-        return graph_evaluation(data, graph, **kwargs)
+        return graph_evaluation(data, adj_matrix, **kwargs)
     else:
-        output = Parallel(n_jobs=nb_jobs)(delayed(graph_evaluation)(data, graph,
+        output = Parallel(n_jobs=nb_jobs)(delayed(graph_evaluation)(data, adj_matrix,
                                           idx=run, gpu_id=run % SETTINGS.GPU,
                                           **kwargs) for run in range(nb_runs))
         return np.mean(output)
@@ -142,8 +143,10 @@ def parallel_graph_evaluation(data, graph, nb_runs=16,
 
 def hill_climbing(data, graph, **kwargs):
     """Hill Climbing optimization: a greedy exploration algorithm."""
-    tested_candidates = [nx.adj_matrix(graph, weight=None)]
-    best_score = parallel_graph_evaluation(data, graph, ** kwargs)
+    nodelist = list(data.columns)
+    data = scale(data.as_matrix()).astype('float32')
+    tested_candidates = [nx.adj_matrix(graph, nodelist=nodelist, weight=None)]
+    best_score = parallel_graph_evaluation(data, tested_candidates[0].todense(), ** kwargs)
     best_candidate = graph
     can_improve = True
     while can_improve:
@@ -152,11 +155,11 @@ def hill_climbing(data, graph, **kwargs):
             test_graph = deepcopy(best_candidate)
             test_graph.add_edge(j, i, weight=test_graph[i][j]['weight'])
             test_graph.remove_edge(i, j)
-            tadjmat = nx.adj_matrix(test_graph, weight=None)
+            tadjmat = nx.adj_matrix(test_graph, nodelist=nodelist, weight=None)
             if (nx.is_directed_acyclic_graph(test_graph) and not any([(tadjmat != cand).nnz ==
                                                                       0 for cand in tested_candidates])):
                 tested_candidates.append(tadjmat)
-                score = parallel_graph_evaluation(data, test_graph, **kwargs)
+                score = parallel_graph_evaluation(data, tadjmat.todense(), **kwargs)
                 if score < best_score:
                     can_improve = True
                     best_candidate = test_graph
@@ -205,7 +208,7 @@ class CGNN(GraphModel):
                           and nx.is_directed_acyclic_graph(nx.DiGraph(np.reshape(np.array(i), (nb_vars, nb_vars)))))]
 
         warnings.warn("A total of {} graphs will be evaluated.".format(len(candidates)))
-        scores = [parallel_graph_evaluation(data, nx.DiGraph(i), nh=nh, nb_runs=nb_runs, gpu=gpu,
+        scores = [parallel_graph_evaluation(data, i, nh=nh, nb_runs=nb_runs, gpu=gpu,
                                             nb_jobs=nb_jobs, lr=lr, train_epochs=train_epochs, test_epochs=test_epochs, verbose=verbose) for i in candidates]
         final_candidate = candidates[scores.index(min(scores))]
         output = np.zeros(final_candidate.shape)
@@ -233,7 +236,6 @@ class CGNN(GraphModel):
         nb_jobs, verbose, gpu = SETTINGS.get_default(('nb_jobs', nb_jobs), ('verbose', verbose), ('gpu', gpu))
         alg_dic = {'HC': hill_climbing, 'HCr': hill_climbing_with_removal,
                    'tabu': tabu_search, 'EHC': exploratory_hill_climbing}
-        data = scale(data.as_matrix()).astype('float32')
 
         return alg_dic[alg](data, dag, nh=nh, nb_runs=nb_runs, gpu=gpu,
                             nb_jobs=nb_jobs, lr=lr, train_epochs=train_epochs,
@@ -249,8 +251,6 @@ class CGNN(GraphModel):
         """
         warnings.warn("The pairwise GNN model is computed on each edge of the UMG "
                       "to initialize the model and start CGNN with a DAG")
-        if verbose:
-            raise ValueError
         nb_jobs, verbose, gpu = SETTINGS.get_default(('nb_jobs', nb_jobs), ('verbose', verbose), ('gpu', gpu))
         gnn = GNN(nh=nh, lr=lr)
 
@@ -258,7 +258,7 @@ class CGNN(GraphModel):
                               nb_jobs=nb_jobs, train_epochs=train_epochs,
                               test_epochs=test_epochs, verbose=verbose, gpu=gpu)  # Pairwise method
         # print(nx.adj_matrix(og).todense().shape)
-
+        # print(list(og.edges()))
         dag = dagify_min_edge(og)
         # print(nx.adj_matrix(dag).todense().shape)
 

cdt/utils/loss.py
@@ -52,8 +52,8 @@ class MMDloss(th.nn.Module):
         s = th.cat([th.ones([input_size, 1]) / input_size,
                     th.ones([input_size, 1]) / -input_size], 0)
 
-        self.S = s.mm(s.t()).to(device)
-
+        self.S = (s @ s.t()).to(device)
+        
     def forward(self, x, y):
         """Compute the MMD statistic between x and y."""
         X = th.cat([x, y], 0)
@@ -61,13 +61,16 @@ class MMDloss(th.nn.Module):
         XX = X @ X.t()
         # dot product of rows with themselves
         # Old code : X2 = (X * X).sum(dim=1)
-        X2 = XX.diag().unsqueeze(0)
+        # X2 = XX.diag().unsqueeze(0)
+        X2 = (X * X).sum(dim=1).unsqueeze(0)
+        # print(X2.shape)
+        # raise ValueError
         # exponent entries of the RBF kernel (without the sigma) for each
         # combination of the rows in 'X'
         exponent = -2*XX + X2.expand_as(XX) + X2.t().expand_as(XX)
-
-        lossMMD = th.sum(self.S * sum([(exponent * -bandwith).exp() for bandwith in self.bandwiths]))
-        return lossMMD.sqrt()
+        
+        lossMMD = th.sum(sum([self.S *(exponent * -bandwith).exp() for bandwith in self.bandwiths]))
+        return lossMMD
 
 
 class MomentMatchingLoss_th(th.nn.Module):