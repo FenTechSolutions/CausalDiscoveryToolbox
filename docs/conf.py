f78cc59d91711c0c117aa1ba5cac4b13fc8739f0
Diviyan Kalainathan <dkalainathan@gmail.com>
2018-08-24 12:47:58 +0200

    [DOC] conf.py tweaks

--- docs/conf.py
+++ docs/conf.py
@@ -191,10 +191,7 @@ mathjax_config = {
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
-github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
 def setup(app):
     app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
-            'auto_toc_tree_section': 'Contents',
             }, True)
     app.add_transform(AutoStructify)
