95c1b21ae8d56e2d5aa7b69327b89790714a17df
Travis CI Bumpversion <travis-bumpversion@example.com>
2019-09-25 09:40:31 +0000

    [DEPLOY] 0.5.11 [skip travis-ci]

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Diviyan Kalainathan, Olivier Goudet'
 # The short X.Y version
 # version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.5.10'
+release = '0.5.11'
 version = release
 
 # -- General configuration ---------------------------------------------------
