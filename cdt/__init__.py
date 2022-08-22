096a93bdd71f61e2299493f29c2d2054ac955d97
CircleCI Bumpversion <ci@circleci.com>
2022-08-22 23:55:04 +0000

    [DEPLOY] 0.6.0 [skip ci]

--- cdt/__init__.py
+++ cdt/__init__.py
@@ -32,4 +32,4 @@ import cdt.metrics
 from cdt.utils import loss
 from cdt.utils.Settings import SETTINGS
 from cdt.utils.R import RPackages
-__version__ = '0.5.24'
+__version__ = '0.6.0'
--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'Diviyan Kalainathan, Olivier Goudet'
 # The short X.Y version
 # version = '0.4'
 # The full version, including alpha/beta/rc tags
-release = '0.5.24'
+release = '0.6.0'
 version = release
 
 # -- General configuration ---------------------------------------------------
